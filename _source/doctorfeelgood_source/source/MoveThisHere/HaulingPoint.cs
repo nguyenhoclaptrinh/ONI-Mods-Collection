8f1c691bbabb842658b495c8e11fd02015151045
nguyenhoclaptrinh <thainguyentruongpl2@gmail.com>
2026-06-06 09:14:45 +0700

    refactor(movethishere): loai bo FilteredStorageHaulingPoint trung lap va dung FilteredStorage goc cua game

--- _source/doctorfeelgood_source/source/MoveThisHere/HaulingPoint.cs
+++ _source/doctorfeelgood_source/source/MoveThisHere/HaulingPoint.cs
@@ -98,7 +98,7 @@ namespace MoveThisHere
         }
 
 
-        protected FilteredStorageHaulingPoint filteredStorage;
+        protected FilteredStorage filteredStorage;
 
         public string choreTypeID = Db.Get().ChoreTypes.StorageFetch.Id;
 
@@ -122,9 +122,7 @@ namespace MoveThisHere
 
             forbidden_tags = (allowManualPumpingStationFetching ? new Tag[0] : new Tag[2] { GameTags.LiquidSource, GameTags.GasSource });
 
-            filteredStorage = new FilteredStorageHaulingPoint(this, forbidden_tags, null, use_logic_meter, fetch_chore_type);
-            //replacing capacity_control slider in filteredstorage - leave it null and do the logic for it here
-            //forbidden tags contains either nothing or pump stations. have to make own copy of filteredstorage just to keep this private field updated
+            filteredStorage = new FilteredStorage(this, forbidden_tags, null, use_logic_meter, fetch_chore_type);
 
             Subscribe((int)GameHashes.CopySettings, OnCopySettingsDelegate);
             Subscribe((int)GameHashes.RefreshUserMenu, OnRefreshUserMenuDelegate);
@@ -144,8 +142,16 @@ namespace MoveThisHere
 
             storage.allowItemRemoval = allowItemRemoval;
 
-            forbidden_tags = (allowManualPumpingStationFetching ? new Tag[0] : new Tag[2] { GameTags.LiquidSource, GameTags.GasSource });
-            filteredStorage.SetForbiddenTags(forbidden_tags);
+            if (allowManualPumpingStationFetching)
+            {
+                filteredStorage.RemoveForbiddenTag(GameTags.LiquidSource);
+                filteredStorage.RemoveForbiddenTag(GameTags.GasSource);
+            }
+            else
+            {
+                filteredStorage.AddForbiddenTag(GameTags.LiquidSource);
+                filteredStorage.AddForbiddenTag(GameTags.GasSource);
+            }
             filteredStorage.FilterChanged();
 
         }
@@ -153,8 +159,16 @@ namespace MoveThisHere
         {
             allowManualPumpingStationFetching = !allowManualPumpingStationFetching;
 
-            forbidden_tags = (allowManualPumpingStationFetching ? new Tag[0] : new Tag[2] { GameTags.LiquidSource, GameTags.GasSource });
-            filteredStorage.SetForbiddenTags(forbidden_tags);
+            if (allowManualPumpingStationFetching)
+            {
+                filteredStorage.RemoveForbiddenTag(GameTags.LiquidSource);
+                filteredStorage.RemoveForbiddenTag(GameTags.GasSource);
+            }
+            else
+            {
+                filteredStorage.AddForbiddenTag(GameTags.LiquidSource);
+                filteredStorage.AddForbiddenTag(GameTags.GasSource);
+            }
             filteredStorage.FilterChanged();
         }
         private void OnChangeWillSpill()
@@ -255,8 +269,16 @@ namespace MoveThisHere
                         allowItemRemoval = component.allowItemRemoval;
                         storage.allowItemRemoval = allowItemRemoval;
                         allowManualPumpingStationFetching = component.allowManualPumpingStationFetching;
-                        forbidden_tags = (allowManualPumpingStationFetching ? new Tag[0] : new Tag[2] { GameTags.LiquidSource, GameTags.GasSource });
-                        filteredStorage.SetForbiddenTags(forbidden_tags);
+                        if (allowManualPumpingStationFetching)
+                        {
+                            filteredStorage.RemoveForbiddenTag(GameTags.LiquidSource);
+                            filteredStorage.RemoveForbiddenTag(GameTags.GasSource);
+                        }
+                        else
+                        {
+                            filteredStorage.AddForbiddenTag(GameTags.LiquidSource);
+                            filteredStorage.AddForbiddenTag(GameTags.GasSource);
+                        }
                         filteredStorage.FilterChanged();
                     }
                 }
@@ -311,9 +333,16 @@ namespace MoveThisHere
                     targetHaulingPoint.allowItemRemoval = allowItemRemoval;
                     targetHaulingPoint.storage.allowItemRemoval = allowItemRemoval;
                     targetHaulingPoint.allowManualPumpingStationFetching = allowManualPumpingStationFetching;
-                    Tag[] forbidden_tags = (allowManualPumpingStationFetching ? new Tag[0] : new Tag[2] { GameTags.LiquidSource, GameTags.GasSource });   
-                    targetHaulingPoint.forbidden_tags = forbidden_tags;
-                    targetHaulingPoint.filteredStorage.SetForbiddenTags(forbidden_tags);
+                    if (allowManualPumpingStationFetching)
+                    {
+                        targetHaulingPoint.filteredStorage.RemoveForbiddenTag(GameTags.LiquidSource);
+                        targetHaulingPoint.filteredStorage.RemoveForbiddenTag(GameTags.GasSource);
+                    }
+                    else
+                    {
+                        targetHaulingPoint.filteredStorage.AddForbiddenTag(GameTags.LiquidSource);
+                        targetHaulingPoint.filteredStorage.AddForbiddenTag(GameTags.GasSource);
+                    }
                     targetHaulingPoint.filteredStorage.FilterChanged();
                 }
             }
@@ -474,254 +503,7 @@ namespace MoveThisHere
     }
 
 
-    public class FilteredStorageHaulingPoint
-    {
-        //this class is basically a copy of filteredstorage with just a few changes necessary to make hauling points work properly
-        //for example, handling of forbidden tags for the auto bottler
 
-        public static readonly HashedString FULL_PORT_ID = "FULL";
-
-        private KMonoBehaviour root;
-
-        private FetchList2 fetchList;
-
-        private IUserControlledCapacity capacityControl;
-
-        private TreeFilterable filterable;
-
-        private Storage storage;
-
-        private MeterController meter;
-
-        private MeterController logicMeter;
-
-        //private Tag[] requiredTags;
-
-        private Tag[] forbiddenTags;
-
-        private bool hasMeter = true;
-
-        private bool useLogicMeter;
-
-        private ChoreType choreType;
-
-        public void SetHasMeter(bool has_meter)
-        {
-            hasMeter = has_meter;
-        }
-
-        public FilteredStorageHaulingPoint(KMonoBehaviour root, Tag[] forbidden_tags, IUserControlledCapacity capacity_control, bool use_logic_meter, ChoreType fetch_chore_type)
-        {
-            this.root = root;
-            forbiddenTags = forbidden_tags;
-            capacityControl = capacity_control;
-            useLogicMeter = use_logic_meter;
-            choreType = fetch_chore_type;
-            root.Subscribe(-1697596308, OnStorageChanged);
-            root.Subscribe(-543130682, OnUserSettingsChanged);
-            filterable = root.FindOrAdd<TreeFilterable>();
-            TreeFilterable treeFilterable = filterable;
-            treeFilterable.OnFilterChanged = (Action<HashSet<Tag>>)Delegate.Combine(treeFilterable.OnFilterChanged, new Action<HashSet<Tag>>(OnFilterChanged));
-            storage = root.GetComponent<Storage>();
-            storage.Subscribe(644822890, OnOnlyFetchMarkedItemsSettingChanged);
-            storage.Subscribe(-1852328367, OnFunctionalChanged);
-        }
-
-        private void OnOnlyFetchMarkedItemsSettingChanged(object data)
-        {
-            OnFilterChanged(filterable.GetTags());
-        }
-
-        private void CreateMeter()
-        {
-            if (hasMeter)
-            {
-                meter = new MeterController(root.GetComponent<KBatchedAnimController>(), "meter_target", "meter", Meter.Offset.Infront, Grid.SceneLayer.NoLayer, "meter_frame", "meter_level");
-            }
-        }
-
-        private void CreateLogicMeter()
-        {
-            if (hasMeter)
-            {
-                logicMeter = new MeterController(root.GetComponent<KBatchedAnimController>(), "logicmeter_target", "logicmeter", Meter.Offset.Infront, Grid.SceneLayer.NoLayer);
-            }
-        }
-
-        public void CleanUp()
-        {
-            if (filterable != null)
-            {
-                TreeFilterable treeFilterable = filterable;
-                treeFilterable.OnFilterChanged = (Action<HashSet<Tag>>)Delegate.Remove(treeFilterable.OnFilterChanged, new Action<HashSet<Tag>>(OnFilterChanged));
-            }
-            if (fetchList != null)
-            {
-                fetchList.Cancel("Parent destroyed");
-            }
-        }
-
-        public void FilterChanged()
-        {
-            if (hasMeter)
-            {
-                if (meter == null)
-                {
-                    CreateMeter();
-                }
-                if (logicMeter == null && useLogicMeter)
-                {
-                    CreateLogicMeter();
-                }
-            }
-            OnFilterChanged(filterable.GetTags());
-            UpdateMeter();
-        }
-
-        private void OnUserSettingsChanged(object data)
-        {
-            OnFilterChanged(filterable.GetTags());
-            UpdateMeter();
-        }
-
-        private void OnStorageChanged(object data)
-        {
-            if (fetchList == null)
-            {
-                OnFilterChanged(filterable.GetTags());
-            }
-            UpdateMeter();
-        }
-
-        private void OnFunctionalChanged(object data)
-        {
-            OnFilterChanged(filterable.GetTags());
-        }
-
-        private void UpdateMeter()
-        {
-            float maxCapacityMinusStorageMargin = GetMaxCapacityMinusStorageMargin();
-            float positionPercent = Mathf.Clamp01(GetAmountStored() / maxCapacityMinusStorageMargin);
-            if (meter != null)
-            {
-                meter.SetPositionPercent(positionPercent);
-            }
-        }
-
-        public bool IsFull()
-        {
-            float maxCapacityMinusStorageMargin = GetMaxCapacityMinusStorageMargin();
-            float num = Mathf.Clamp01(GetAmountStored() / maxCapacityMinusStorageMargin);
-            if (meter != null)
-            {
-                meter.SetPositionPercent(num);
-            }
-            if (!(num >= 1f))
-            {
-                return false;
-            }
-            return true;
-        }
-
-        private void OnFetchComplete()
-        {
-            OnFilterChanged(filterable.GetTags());
-        }
-
-        private float GetMaxCapacity()
-        {
-            float num = storage.capacityKg;
-            if (capacityControl != null)
-            {
-                num = Mathf.Min(num, capacityControl.UserMaxCapacity);
-            }
-            return num;
-        }
-
-        private float GetMaxCapacityMinusStorageMargin()
-        {
-            return GetMaxCapacity() - storage.storageFullMargin;
-        }
-
-        private float GetAmountStored()
-        {
-            float result = storage.MassStored();
-            if (capacityControl != null)
-            {
-                result = capacityControl.AmountStored;
-            }
-            return result;
-        }
-
-        private bool IsFunctional()
-        {
-            Operational component = storage.GetComponent<Operational>();
-            if (!(component == null))
-            {
-                return component.IsFunctional;
-            }
-            return true;
-        }
-
-        public void SetForbiddenTags(Tag[] forbidden_tags)
-        {
-            forbiddenTags = forbidden_tags; //wouldn't need this whole class except for that
-                                            //and actually, after the update 10/4 which added new public methods to modify forbidden tags, may well be entirely unnecessary
-                                            //but... if it ain't broke, I'm not fixing it
-        }
-
-        private void OnFilterChanged(HashSet<Tag> tags)
-        {
-            bool flag = tags != null && tags.Count != 0;
-            if (fetchList != null)
-            {
-                fetchList.Cancel("");
-                fetchList = null;
-            }
-            float maxCapacityMinusStorageMargin = GetMaxCapacityMinusStorageMargin();
-            float amountStored = GetAmountStored();
-            float num = Mathf.Max(0f, maxCapacityMinusStorageMargin - amountStored);
-            if (num > 0f && flag && IsFunctional())
-            {
-                num = Mathf.Max(0f, GetMaxCapacity() - amountStored);
-                fetchList = new FetchList2(storage, choreType);
-                fetchList.ShowStatusItem = false;
-                fetchList.Add(tags, forbiddenTags, num, Operational.State.Functional);
-                fetchList.Submit(OnFetchComplete, check_storage_contents: false);
-            }
-        }
-
-        public void SetLogicMeter(bool on)
-        {
-            if (logicMeter != null)
-            {
-                logicMeter.SetPositionPercent(on ? 1f : 0f);
-            }
-        }
-        /*public void AddForbiddenTag(Tag forbidden_tag)
-		{
-			if (forbiddenTags == null)
-			{
-				forbiddenTags = new Tag[0];
-			}
-			if (!forbiddenTags.Contains(forbidden_tag))
-			{
-				forbiddenTags = forbiddenTags.Append(forbidden_tag);
-				OnFilterChanged(filterable.GetTags());
-			}
-		}
-
-		public void RemoveForbiddenTag(Tag forbidden_tag)
-		{
-			if (forbiddenTags != null)
-			{
-				List<Tag> list = new List<Tag>(forbiddenTags);
-				list.Remove(forbidden_tag);
-				forbiddenTags = list.ToArray();
-				OnFilterChanged(filterable.GetTags());
-			}
-		}*/
-    }
 
 
 }
